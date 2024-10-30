3a5d038b2faa49f71685e65970cc048acb7fb2a8
GuillePT95 <guillermopereztoba@gmail.com>
2024-10-30 21:00:13 +0100

    modificaciones y nuevos archivos

---
+++ env.py
@@ -0,0 +1,11 @@
+import os
+from dotenv import load_dotenv
+
+# Cargar las variables de entorno desde el archivo .env
+load_dotenv()
+
+#Mail = os.getenv("Mail")
+#Mail_password = os.getenv("Mail_password")
+
+#print("Mail:", Mail)
+#print("Mail_password:", Mail_password)
